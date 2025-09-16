6d8554be1b57dd332123a9cbcc86d5518d2b4151
Chisomo Kanzengo <chisokanzengo@gmail.com>
2025-09-16 20:50:34 +0200

    Fix: Force update of db.py for PostgreSQL

--- db.py
+++ db.py
@@ -1,23 +1,20 @@
+# The correct db.py file
 import psycopg2
 from flask import g
 from config import Config
 
-# This function will now get the connection from the request context 'g'
-# or create it if it doesn't exist for the current request.
 def get_db_connection():
     if 'db' not in g:
-        # Connect to PostgreSQL using the credentials from config.py
         g.db = psycopg2.connect(
             host=Config.DB_HOST,
             port=Config.DB_PORT,
             user=Config.DB_USER,
             password=Config.DB_PASSWORD,
             database=Config.DB_NAME,
-            sslmode='require'  # This is often required for cloud databases like Supabase
+            sslmode='require'
         )
     return g.db
 
-# This function will be called from app.py to close the connection
 def close_db(e=None):
     db = g.pop('db', None)
     if db is not None:
