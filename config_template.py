25867b4de7d4b1f2b45a76550cf20ddd5242eed4
Chisomo Kanzengo <chisokanzengo@gmail.com>
2025-09-16 16:58:45 +0200

    Update database connection to Supabase

--- config_template.py
+++ config_template.py
@@ -1,7 +1,8 @@
 class Config:
     SECRET_KEY = "your-flask-secret-key-goes-here"
     DB_HOST = "your-database-host-from-render"
+    DB_PORT = 5432  # <-- Add this line
     DB_USER = "your-database-user-from-render"
     DB_PASSWORD = "your-database-password-from-render"
     DB_NAME = "your-database-name-from-render"
-    DEBUG = False # False for production
+    DEBUG = False  # False for production
